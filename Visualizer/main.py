adc4c3daf07e813383b856e520da8295da1366ca
Severn Everett <severn.everett@gmail.com>
2022-12-29 08:10:23 -0800

    Fixing chart formatting; Removing unnecessary x-axis label parameter

--- Visualizer/main.py
+++ Visualizer/main.py
@@ -57,7 +57,6 @@ def parse_file(file_name):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a bar chart from results.')
     parser.add_argument('-f', '--file', type=str, help='the data file')
-    parser.add_argument('-x', '--x_label', type=str, help='the x-axis label')
     parser.add_argument('-y', '--y_label', type=str, help='the y-axis label')
     args = parser.parse_args()
 
@@ -66,13 +65,15 @@ if __name__ == '__main__':
     for er in exception_runs:
         plt.errorbar(ppm, er.results, label=er.title, yerr=er.error_ranges)
 
-    plt.xscale("log")
-    plt.yscale("log")
+    plt.xscale('log')
+    plt.yscale('log')
     major_formatter = StrMethodFormatter('{x:.0f}')
     plt.gca().get_yaxis().set_major_formatter(major_formatter)
     plt.gca().get_xaxis().set_major_formatter(major_formatter)
-    plt.xlabel(args.x_label, fontsize='large', fontweight='bold')
-    plt.ylabel(args.y_label, fontsize='large', fontweight='bold')
+    label_size = 'x-large'
+    plt.xlabel('PPM', fontsize=label_size, fontweight='bold')
+    plt.ylabel(args.y_label, fontsize=label_size, fontweight='bold')
+    plt.grid(visible=True, color='0.8')
     plt.legend()
     plt.tight_layout()
 
